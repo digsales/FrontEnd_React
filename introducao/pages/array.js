5354a5da2e3d58a9d1c442a282570bb16c12608e
Diogo Sales <diogodobu@gmail.com>
2023-04-04 21:40:52 -0300

    Add objeto page and edit array page

--- introducao/pages/array.js
+++ introducao/pages/array.js
@@ -4,11 +4,25 @@ import { Container } from "react-bootstrap";
 import Pagina from "../components/Pagina";
 
 const array = () => {
+  const carros = ["Celton", "Uno", "Tempra", "Civic", "Marea", "Doblô"];
+
+  // const pessoa = { nome: "Orion", idade: 21, telefone: "(61) 98765-4321" };
+
+  // const nome = "Orion";
+  // const idade = 21;
+  // const telefone = "(61) 98765-4321";
+
   return (
     <>
       <Pagina titulo="Arrays">
         <Container>
-          <p>Arrays</p>
+          <h1>Lista Ordenada</h1>
+          <br />
+          {carros.map((item, index) => (
+            <p>
+              {index + 1} - {item}
+            </p>
+          ))}
         </Container>
       </Pagina>
     </>
---
+++ introducao/pages/objeto.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Pagina from "../components/Pagina";
+
+const objeto = () => {
+  return (
+    <Pagina titulo="Objetos">
+      <Container>
+        <p>Objeto</p>
+      </Container>
+    </Pagina>
+  );
+};
+
+export default objeto;
